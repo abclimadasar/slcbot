9123c91308447a2388bf89d8ee416cfa5f5fc27a
Abdul Mufid <110360966+eabdalmufid@users.noreply.github.com>
2022-11-21 20:25:42 +0700

    New

--- plugins/owner-boton.js
+++ plugins/owner-boton.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('*Sukses Menghidupkan FANGZ BOT ✅*')
+    m.reply('*Sukses Menghidupkan SILENCE BOT ✅*')
 }
 handler.command = /^(boton)$/i
 handler.owner = true
